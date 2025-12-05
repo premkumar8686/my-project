06af86add3b3bdd68b407d5bba9e7cca2e46226f
premkumar8686 <premkumarweb0369@gmail.com>
2025-12-05 12:54:50 +0530

    Setup

---
+++ src/app/next-admin/page.tsx
@@ -0,0 +1,16 @@
+export default function AdminLogin() {
+  return (
+    <section className="bg-[#E7E2FF] h-screen flex justify-center items-center">
+        {/* Row Section Start */}
+        <div className="h-[70vh] w-[80%] bg-white rounded-xl shadow-[0px_10px_15px_-3px_rgba(0,0,0,0.1)] flex">
+            <div className="w-1/2 rounded-s-xl bg-[url('/images/man-reading-news-paper.webp')] bg-cover bg-center">
+            </div>
+            <div className="w-1/2">
+            
+            </div>
+        </div>
+        {/* Row Section End */}
+    </section>
+
+  )
+}
